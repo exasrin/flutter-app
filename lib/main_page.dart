4c62c0334880d84e3446247b88226ee4697fe553
asrin <asrinasinua@gmail.com>
2024-03-11 14:38:15 +0700

    [ADD] container box decoration

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -10,39 +10,31 @@ class MainPage extends StatelessWidget {
           title: const Text("Aplikasi Pertamaku"),
           backgroundColor: Colors.blue,
         ),
-        body: Column(
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red,
-                  margin: const EdgeInsets.only(left: 10, right: 10),
-                  padding: EdgeInsets.all(10),
-                  child: Container(
-                    color: Colors.orange,
-                  ),
-                ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.yellow,
-                )
-              ],
-            ),
-            Row(
-              children: [
-                Container(width: 100, height: 100, color: Colors.green),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.blue,
-                )
-              ],
-            )
-          ],
+        body: Center(
+          child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                  // color: Colors.red,
+                  border: Border.all(color: Colors.black, width: 3),
+                  // borderRadius: const BorderRadius.only(
+                  //     topLeft: Radius.circular(20),
+                  //     bottomRight: Radius.circular(20)),
+                  // gradient: const LinearGradient(
+                  //     colors: [Colors.red, Colors.yellow],
+                  //     begin: Alignment.topLeft,
+                  //     end: Alignment.bottomRight)),
+                  image: const DecorationImage(
+                      image: NetworkImage("https://picsum.photos/200/300"),
+                      fit: BoxFit.cover),
+                  boxShadow: const [
+                    BoxShadow(
+                        color: Colors.black45,
+                        blurRadius: 6,
+                        spreadRadius: 3,
+                        offset: Offset(2, 2))
+                  ],
+                  shape: BoxShape.circle)),
         ));
   }
 }
