fc6c500349666453827636af1f561579050f4d4f
asrin <asrinasinua@gmail.com>
2024-03-16 06:48:30 +0700

    [ADD] change font style use google font

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -15,27 +15,6 @@ class MainPage extends StatelessWidget {
             width: 200,
             child: Text(
               "Hello My Name Is Alffathir Rasyid Sulaiaman",
-              textAlign: TextAlign.start,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                  backgroundColor: Colors.red,
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic,
-                  shadows: [
-                    Shadow(
-                        blurRadius: 5,
-                        color: Colors.black,
-                        offset: Offset(3, 3))
-                  ],
-                  // letterSpacing: 5,
-                  // wordSpacing: 5
-                  decoration: TextDecoration.lineThrough,
-                  decorationColor: Colors.black,
-                  decorationThickness: 4,
-                  decorationStyle: TextDecorationStyle.wavy),
             ),
           ),
         ));
