c1bd7785b80c2c9538bb2740a897576794aabb6e
asrin <asrinasinua@gmail.com>
2024-03-16 14:10:03 +0700

    [ADD] align

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -13,37 +10,21 @@ class MainPage extends StatelessWidget {
           backgroundColor: Colors.grey.shade200,
           title: const Center(
               child: Text(
-            "Wrap Demo",
+            "Align Demo",
             style: TextStyle(fontWeight: FontWeight.bold),
           )),
         ),
-        body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          color: Colors.grey.shade200,
-          child: Wrap(
-              alignment: WrapAlignment.center, //secara horizontal
-              runAlignment: WrapAlignment.end, //secara vertical
-              crossAxisAlignment:
-                  WrapCrossAlignment.center, //di dalam baris wrap
-              verticalDirection: VerticalDirection.up, // arah mulainya
-              // direction: Axis.vertical,
-              spacing: 10,
-              runSpacing: 20,
-              children: List.generate(
-                  8,
-                  (index) => Container(
-                        width: index % 2 == 0 ? 150 : 70,
-                        height: index % 2 == 0 ? 150 : 70,
-                        color: index % 2 == 0 ? Colors.red : Colors.blue,
-                        child: Center(
-                          child: Text(
-                            index.toString(),
-                            style: const TextStyle(
-                                fontSize: 30, color: Colors.white),
-                          ),
-                        ),
-                      ))),
+        body: const Align(
+          // alignment: Alignment.center,
+          alignment: Alignment(0, -0.5),
+          child: SizedBox(
+            width: 300,
+            child: Text(
+              "Hello everyone, my name is alfaathir rasyid sulaiaman and im from oheo southeast seulawesi",
+              textAlign: TextAlign.justify,
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+            ),
+          ),
         ));
   }
 }
