d78fe9460619c24f42f7cd93119010c59c2ecb51
asrin <asrinasinua@gmail.com>
2024-03-16 13:29:19 +0700

    [ADD] layout widget stack

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,64 +1,56 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class MainPage extends StatefulWidget {
+class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  int number = 0;
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade200,
+        title: const Center(
+            child: Text(
+          "Stack Demo",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        )),
+      ),
+      body: Container(
+        height: 300,
+        width: 300,
+        color: Colors.yellow.shade200,
+        child: Stack(
+          alignment: Alignment.topRight,
           children: [
-            Text(
-              '$number',
-              style: GoogleFonts.poppins(fontSize: 40),
+            Container(
+              color: Colors.grey.withOpacity(0.3),
+              child: Container(
+                margin: const EdgeInsets.all(0),
+                height: 100,
+                width: 100,
+                color: Colors.red,
+              ),
             ),
-            const SizedBox(
-              height: 20,
+            Container(
+              color: Colors.grey.withOpacity(0.3),
+              child: Container(
+                margin: const EdgeInsets.all(20),
+                height: 100,
+                width: 100,
+                color: Colors.green,
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      number++;
-                    });
-                  },
-                  child: const Text("+"),
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      number = 0;
-                    });
-                  },
-                  child: const Text("Reset"),
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      number <= 0 ? number = 0 : number--;
-                    });
-                  },
-                  child: const Text("-"),
-                )
-              ],
-            )
+            Container(
+              color: Colors.grey.withOpacity(0.3),
+              child: Container(
+                margin: const EdgeInsets.all(40),
+                height: 100,
+                width: 100,
+                color: Colors.blue,
+              ),
+            ),
           ],
         ),
       ),
